0a0b7c1078f8622887d9bd875a4776e7329d1967
sasahara_t_alps <tadayuki-sasahara@alpsgiken.co.jp>
2015-12-28 19:24:17 +0900

    [N1]jarファイルの不具合修正
    ・android-support.jarがあると動かなかったので、削除致しました。

---
+++ CardboardSample/src/main/java/com/google/vrtoolkit/cardboard/samples/treasurehunt/CubeControl.java
@@ -0,0 +1,13 @@
+package com.google.vrtoolkit.cardboard.samples.treasurehunt;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import jp.co.altec.openingactionsample.DeviceInfo;
+import jp.co.altec.openingactionsample.Point;
+
+/**
+ * Created by 2015295 on 2015/12/28.
+ */
+public class CubeControl {
+    public static ConcurrentHashMap<String, Cube> mCubeInfos = new ConcurrentHashMap<>();
+}
